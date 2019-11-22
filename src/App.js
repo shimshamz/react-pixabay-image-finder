a329f5d0cc856acf1b0dc1285c236ab126dc3140
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-11-21 19:57:40 -0500

    Edit CSS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import NavBar from './components/navbar/NavBar';
 import Search from './components/search/Search';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
         <div>
           <NavBar />
-          <Search />
+          <div className="container">
+            <Search />
+          </div>
         </div>
       </MuiThemeProvider>
     )
   }
 }
 
+export default App;
