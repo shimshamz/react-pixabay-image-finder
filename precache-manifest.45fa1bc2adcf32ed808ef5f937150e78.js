68294e6fe55eef03edc9da72679a42a7580151ec
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-11-30 20:38:17 -0500

    Updates

---
+++ precache-manifest.45fa1bc2adcf32ed808ef5f937150e78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95c32b0b8d5b3c76e72545ad38c87f9",
+    "url": "/react-pixabay-image-finder/index.html"
+  },
+  {
+    "revision": "851ffea98f8a8da5c21b",
+    "url": "/react-pixabay-image-finder/static/css/main.80ac0d44.chunk.css"
+  },
+  {
+    "revision": "9a36392345c0d97c05e9",
+    "url": "/react-pixabay-image-finder/static/js/2.be5578c2.chunk.js"
+  },
+  {
+    "revision": "851ffea98f8a8da5c21b",
+    "url": "/react-pixabay-image-finder/static/js/main.adae8271.chunk.js"
+  },
+  {
+    "revision": "3082fde0342696871687",
+    "url": "/react-pixabay-image-finder/static/js/runtime-main.8f81e067.js"
+  }
+]);
