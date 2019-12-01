a5ebff5e672ccf4030b4e0161a5695d16ffe0c0b
shimshamz <31662802+shimshamz@users.noreply.github.com>
2019-11-30 20:24:48 -0500

    Commented console log

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -29,7 +29,7 @@ class Search extends Component {
   onAmountChange = (e, index, value) => this.setState({amount: value});
 
   render() {
-    console.log(this.state.images);
+    //console.log(this.state.images);
     return (
       <div>
         <TextField 
